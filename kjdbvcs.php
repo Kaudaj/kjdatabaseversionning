4bb4cc6a47d81311376832c1be500c60b6435689
Kaudaj <info@kaudaj.com>
2022-01-26 15:16:39 +0100

    Start from kjmodulebedrock

---
+++ kjdbvcs.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Copyright since 2019 Kaudaj
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@kaudaj.com so we can send you a copy immediately.
+ *
+ * @author    Kaudaj <info@kaudaj.com>
+ * @copyright Since 2019 Kaudaj
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ */
+
+declare(strict_types=1);
+
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+}
+
+use Kaudaj\Module\DBVCS\Form\Settings\GeneralConfiguration;
+use PrestaShop\PrestaShop\Adapter\Configuration;
+use PrestaShop\PrestaShop\Adapter\SymfonyContainer;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class KJDBVCS extends Module
+{
+    /**
+     * @var array<string, string> Configuration values
+     */
+    public const CONFIGURATION_VALUES = [
+        GeneralConfiguration::EXAMPLE_SETTING_KEY => 'default_value',
+    ];
+
+    /**
+     * @var string[] Hooks to register
+     */
+    public const HOOKS = [
+        'exampleHook',
+    ];
+
+    /**
+     * @var Configuration<string, mixed> Configuration
+     */
+    private $configuration;
+
+    public function __construct()
+    {
+        $this->name = 'kjdbvcs';
+        $this->tab = 'others';
+        $this->version = '1.0.0';
+        $this->author = 'Kaudaj';
+        $this->ps_versions_compliancy = ['min' => '1.7.8.0', 'max' => _PS_VERSION_];
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->trans('DBVCS', [], 'Modules.Kjdbvcs.Admin');
+        $this->description = $this->trans(<<<EOF
+        Boost module development by providing a solid bedrock.
+EOF
+            ,
+            [],
+            'Modules.Kjdbvcs.Admin'
+        );
+
+        $this->tabs = [
+            [
+                'name' => 'DBVCS Settings',
+                'class_name' => 'DBVCSSettings',
+                'parent_class_name' => 'CONFIGURE',
+                'visible' => false,
+                'wording' => 'DBVCS Settings',
+                'wording_domain' => 'Modules.Kjdbvcs.Admin',
+            ],
+        ];
+
+        $this->configuration = new Configuration();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isUsingNewTranslationSystem(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(): bool
+    {
+        return parent::install()
+            && $this->installConfiguration()
+            && $this->registerHook(self::HOOKS)
+        ;
+    }
+
+    /**
+     * Install configuration values
+     */
+    private function installConfiguration(): bool
+    {
+        try {
+            foreach (self::CONFIGURATION_VALUES as $key => $default_value) {
+                $this->configuration->set($key, $default_value);
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function uninstall(): bool
+    {
+        return parent::uninstall()
+            && $this->uninstallConfiguration()
+        ;
+    }
+
+    /**
+     * Uninstall configuration values
+     */
+    private function uninstallConfiguration(): bool
+    {
+        try {
+            foreach (array_keys(self::CONFIGURATION_VALUES) as $key) {
+                $this->configuration->remove($key);
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get module configuration page content
+     */
+    public function getContent(): void
+    {
+        $container = SymfonyContainer::getInstance();
+
+        if ($container != null) {
+            /** @var UrlGeneratorInterface */
+            $router = $container->get('router');
+
+            Tools::redirectAdmin($router->generate('kj_dbvcs_settings'));
+        }
+    }
+
+    /**
+     * Example hook
+     *
+     * @param array<string, mixed> $params Hook parameters
+     */
+    public function hookExampleHook(array $params): void
+    {
+        /* Do anything */
+    }
+}
